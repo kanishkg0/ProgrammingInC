4e01cb8a64d258cc18027df95550d77f2e4ca564
Kanishk <gkanishk448@gmail.com>
2026-08-20 00:29:51 +0530

    Initial Commit

--- 100_DAYS_OF_CODE/Day 010/Q20_weekdays.c
+++ 100_DAYS_OF_CODE/Day 010/Q20_weekdays.c
@@ -13,31 +13,24 @@ int main() {
     case 1:
         printf("Monday");
         break;
-
     case 2:
         printf("Tuesday");
         break;
-
     case 3:
         printf("Wednesday");
         break;
-
     case 4:
         printf("Thursday");
         break;
-
     case 5:
         printf("Friday");
         break;
-
     case 6:
         printf("Saturday");
         break;
-
     case 7:
         printf("Sunday");
-        break;
-    
+        break;    
     default:
         printf("Invalid number, Please enter a number from (1-7)");
         break;
