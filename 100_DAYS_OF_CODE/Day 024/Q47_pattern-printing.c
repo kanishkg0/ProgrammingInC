a72283225a41ddc7edaed8d991036708d4565a49
Kanishk <gkanishk448@gmail.com>
2026-09-02 13:09:25 +0530

    Initial commit

---
+++ 100_DAYS_OF_CODE/Day 024/Q47_pattern-printing.c
@@ -0,0 +1,28 @@
+/*Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****  */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Emter a number: ");
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+
+return 0;
+    
+}
+
